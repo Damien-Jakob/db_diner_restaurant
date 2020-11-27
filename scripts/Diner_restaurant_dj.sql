b58305bafea23c79e10acdd2d6589fd89e2e623a
Damien Jakob <damien.jakob@cpnv.ch>
2020-11-27 10:29:06 +0100

    Add dishes data and tests

--- scripts/Diner_restaurant_dj.sql
+++ scripts/Diner_restaurant_dj.sql
@@ -152,3 +152,26 @@ INSERT INTO Invoice (invoiceNumber) VALUES (2);
 INSERT INTO Invoice (invoiceNumber) VALUES (3);
 INSERT INTO Invoice (invoiceNumber) VALUES (4);
 INSERT INTO Invoice (invoiceNumber) VALUES (5);
+
+INSERT INTO DishType (idDishType, DishTypeName) VALUES (1, 'Entrées');
+INSERT INTO DishType (idDishType, DishTypeName) VALUES (2, 'Poissons');
+INSERT INTO DishType (idDishType, DishTypeName) VALUES (3, 'Viande');
+INSERT INTO DishType (idDishType, DishTypeName) VALUES (4, 'Fromages');
+INSERT INTO DishType (idDishType, DishTypeName) VALUES (5, 'Dessert');
+
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Risotto tessinois bio aux truffes de Bourgogne et mascarpone', 31, 1);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Gravlax de chevreuil aux citrons confits et câprons', 30, 1);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Terrine de foie gras aux figues et pain d’épices', 29, 1);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Tataki de thon aux pistaches de Bronte', 45, 2);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Scalopines de saumon du Tessin, beurre blanc', 46, 2);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Noix Saint-Jacques et gambas en brochette de romarin', 49, 2);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Médaillons de renne poivrade', 54, 3);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Suprême de canard sauvage à l’orange', 48, 3);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Ris de veau façon saltimbocca à la sauge', 45, 3);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Tournedos de boeuf, vierge automnale aux marrons et pignons', 50, 3);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Selle de chevreuil à la raisineé (dès 2 pers.)', 52, 3);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Assiette de fromages ', 21, 4);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Chaud-froid au chocolat noir, sorbet aux poires', 19, 5);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Opéra au café et chocolat', 20, 5);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Symphonie de crèmes brûlées aux saveurs différentes', 19, 5);
+INSERT INTO Dish (dishDescription, AmountWithTaxes, fkDishType) VALUES ('Bavarois de poires en verrine, émiettée de spéculoos', 19, 5);
--- scripts/test.sql
+++ scripts/test.sql
@@ -3,3 +3,9 @@ GO
 
 -- Should return at lest 5 elements
 SELECT * FROM Invoice;
+
+-- Should be 5
+SELECT Count(*) FROM DishType;
+
+-- Should be 16
+SELECT Count(*) FROM Dish;
