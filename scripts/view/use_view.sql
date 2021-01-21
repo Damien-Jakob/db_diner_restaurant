34999791a4c75bf5404f67f477dc16ab2729f411
Damien Jakob <damien.jakob@cpnv.ch>
2021-01-21 07:30:57 +0100

    Add view notes

---
+++ scripts/view/use_view.sql
@@ -0,0 +1,14 @@
+﻿select * from planificationLendemain
+
+-- INSERT, DELETE, UPDATE : only if single table view
+
+insert into planificationLendemain(firstname, lastname, fktable, datework) 
+values ('Marylou', 'Koume', 6, '2018.12.07')
+
+delete from planificationLendemain 
+where firstname = 'Marylou' and lastName = 'Koume' and datework = '2018.12.07'
+
+update planificationLendemain 
+set firstname = 'Marylou', lastName = 'Koume', datework = '2018.12.07' 
+where firstname = 'Eva' and lastName = 'Risselle' and fkTable = 11
+
