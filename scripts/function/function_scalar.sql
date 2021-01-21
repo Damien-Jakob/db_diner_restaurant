1fd693e8fb8d0a0912a4c16e3059dd647562df9c
Damien Jakob <damien.jakob@cpnv.ch>
2021-01-21 21:22:05 +0100

    Add scalar function

---
+++ scripts/function/function_scalar.sql
@@ -0,0 +1,26 @@
+﻿drop function Recette
+go
+
+-- recette des @duration derniers jours (@duration = 1 -> recette du jour)
+
+-- must have GO before and after
+-- TODO generate error
+CREATE FUNCTION Recette (@duration int)
+returns decimal
+as
+begin
+
+	declare @somme decimal(20,2);
+
+	if @duration < 0 or @duration > 365
+	begin
+		-- hacky way to generate an error in a function
+		return cast('Invalid duration' as int);
+		-- set @somme = -1;
+	end
+	else
+		select @somme = sum(totalAmountWithTaxes) from Invoice where DATEADD(DAY,@duration,invoiceDate) > GETDATE();
+		
+	return @somme;
+end
+go
